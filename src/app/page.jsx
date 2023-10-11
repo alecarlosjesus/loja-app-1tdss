18f3731d955701fcab372c6e5e8f434446a7dc5e
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-11 19:23:18 -0300

    Removendo a pasta post e seus segmentos e links.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-        <div>
-          <p><Link href="/posts">Meu Post...</Link></p>
-          <p><Link href="/posts/post1">Post1...</Link></p>
-          <p><Link href="/posts/post2">Post2...</Link></p>
-          <p><Link href="/posts/post3">Post3...</Link></p>
-        </div>
+  
     </div>
   )
 }
