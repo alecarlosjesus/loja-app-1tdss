358cd7e8ffe8dabba34f7aae02c92ad97a03ac2f
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-18 20:09:47 -0300

    Basics de tailwind - Agrupamento de rotas

--- src/app/produtos/calca/page.jsx
+++ src/app/produtos/calca/page.jsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 
 export default function Calca(){
     return(
-        <div>
-            <h1>CALÇA</h1>
+          
             <div>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi minus ullam in ut, excepturi vel. Reprehenderit optio hic rerum nihil doloribus. Quos ipsam expedita veritatis aspernatur quasi, magnam eius odit.</p>
                 <p><Link href="/">HOME</Link></p>
@@ -12,6 +11,5 @@ export default function Calca(){
                     <Image src="/img/clothes-615×640.png" alt="Calça" width={307} height={320}/>
                 </figure>
             </div>
-        </div>
     )
 }
