380b57e1cb3983faf8a86ae2db1a952a7b0d48e8
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-27 20:23:02 -0300

    Criando o form de login para consumir os dados da API.

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,40 +1,66 @@
+"use client";
+import { useState } from "react";
 
-export default async function LoginUsers() {
+export default function LoginUsers() {
 
-    let users;
+  const [usuario, setUsuario] = useState({
+    email: "",
+    senha: "",
+  });
 
+  const handleChange = (e) => {
+    const{name,value} = e.target;
+    setUsuario({...usuario,[name]:value});
+  };
+
+  const handleSubmit = async (e) => {
     //Recuperar o usuário que está em nossa base json.
-    try{
-        const response = await fetch("http://localhost:3000/api/base/base-user-api");
-        users = await response.json();
-        console.log(users);
-    }catch(error){
-        console.log(error);
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/base/base-user-api",
+        {}
+      );
+
+      console.log(users);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
   return (
     <div>
-        <h1>IDENTIFICAÇÃO DOS USUÁRIOS</h1>
-        
-        <div className="form-login">
-            <form>
-                <fieldset>
-                    <legend>LOGIN</legend>
-                    <div>
-                        <label htmlFor="idUser">Usuário:</label>
-                        <input type="text" name="usuario" id="idUser" placeholder="Digite seu nome de usuário."/>
-                    </div>
-                    <div>
-                        <label htmlFor="idPassword">Senha:</label>
-                        <input type="password" name="senha" id="idPassword" placeholder="Digite sua senha."/>
-                    </div>
-                    <div>
-                        <button>LOGIN</button>
-                    </div>
-                </fieldset>
-            </form>
-        </div>
+      <h1>IDENTIFICAÇÃO DOS USUÁRIOS</h1>
 
+      <div className="form-login">
+        <form>
+          <fieldset>
+            <legend>LOGIN</legend>
+            <div>
+              <label htmlFor="idEmail">Email:</label>
+              <input
+                type="email"
+                name="email"
+                id="idEmail"
+                placeholder="Digite seu email."
+                value={usuario.email} onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="idSenha">Senha:</label>
+              <input
+                type="password"
+                name="senha"
+                id="idSenha"
+                placeholder="Digite sua senha." 
+                value={usuario.senha} onChange={handleChange}
+              />
+            </div>
+            <div>
+              <button>LOGIN</button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
